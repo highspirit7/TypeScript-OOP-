ad304de7caac1ddf4273b31ae13c1170bd4230fe
Jake Lee <jake.lee4006@gmail.com>
2021-04-14 00:08:14 +0900

    Tried to solve game.ts

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,43 @@
 /**
  * Let's make a game 🕹
  */
+
+type Position = {
+	x: number;
+	y: number;
+};
+
+type Direction = "up" | "down" | "left" | "right";
+
+const position: Position = {
+	x: 0,
+	y: 0,
+};
+
+function move(direction: Direction): void {
+	switch (direction) {
+		case "up":
+			position.y += 1;
+			break;
+		case "down":
+			position.y -= 1;
+			break;
+		case "left":
+			position.x -= 1;
+			break;
+		case "right":
+			position.x += 1;
+			break;
+		default:
+			throw new Error("unknown direction!");
+	}
+}
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
